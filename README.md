db7bdcdb4412b90da45b54a3ca360c76594f7bf1
unknown <nagatox60@gmail.com>
2022-11-03 20:37:43 +0100

    another commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hng-console-task
+gh repo clone odinaka60/hng-console-task
+cd into csvTask Folder
+run python script1.py
+open 'HNGi9 CSV FILE - Sheet1.output.csv' file
--- script1.py
+++ script1.py
@@ -4,45 +4,42 @@ import hashlib
  
 def csv_to_json(csv_file_path, new_csv_file_path):
     
-    data_dict = {}
+    hash_list = []
  
     
     #open the csv file 
     with open(csv_file_path, encoding = 'utf-8') as csv_file_handler:
         csv_reader = csv.DictReader(csv_file_handler)
  
-        #convert each row into a dictionary
-        #and add the converted data to the data_variable
- 
+        #convert each row into a dictionary and get hash of each row
+        
+        count = 0
         for rows in csv_reader:
- 
-            
-            key = rows['Serial Number']
-            data_dict[key] = rows
-    #convert the dictionary to json file and create a sha256 of json file
-    the_result = json.dumps(data_dict, indent = 4)
-    the_hash = hashlib.sha256(the_result.encode("utf-8")).hexdigest()
-    print(the_hash)
+            if count == 0:
+                hash_list.append('Hash Value')
+            else:
+                the_result = json.dumps(rows, indent = 4)
+                the_hash = hashlib.sha256(the_result.encode("utf-8")).hexdigest()
+                hash_list.append(the_hash)
+            count = count+1
     # open the input csv file in read mode
     # and open output csv file in write mode
     with open(csv_file_path, 'r') as old_file_handler, open(new_csv_file_path, 'w', newline='') as new_file_handler:
         
         old_file = csv.reader(old_file_handler)
         new_file = csv.writer(new_file_handler)
-        count = 0
+        index = 0
         #add the hash value to each row of the csv file
         for row in old_file:
-            if count == 0:
-                row.append('hash Value')
+            if index<len(hash_list):
+                row.append(hash_list[index])
                 new_file.writerow(row)
-            else:
-                row.append(the_hash)
-                new_file.writerow(row)
-            count = count+1
+                index = index+1
+        print("succesful")
     
  
-csv_file = 'NFT Naming csv - All Teams.csv'
-modified_csv_file = 'NFT Naming csv - All Teams.output.csv'
+csv_file = 'HNGi9 CSV FILE - Sheet1.csv'
+modified_csv_file = 'HNGi9 CSV FILE - Sheet1.output.csv'
  
 if __name__ == '__main__':
     
